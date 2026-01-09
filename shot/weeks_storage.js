89fc1e854e1f987f4f1818281904888dd7a073a5
waftyhubris <declantzammit@gmail.com>
2026-01-09 23:30:02 +1100

    Cleaner mobile formatting

--- shot/weeks_storage.js
+++ shot/weeks_storage.js
@@ -1,4 +1,10 @@
 const week1HTML = `
+    <h2>
+        Week 1: Spectral Sequences
+    </h2>
+    <h3>
+    Date and location to be confirmed
+    </h3>
     <p>
         Abstract: The first talk in our reading group! We will define spectral sequences, and use as our motivating example the spectral sequence of a filtered complex. We will then introduce the spectral sequence of a double complex, and produce quick and marvelous proofs of some results in homological algebra, such as the five lemma and the snake lemma.
     </p>
@@ -11,7 +17,26 @@ const week1HTML = `
     </p>
 `;
 
+const week1HTMLmobile = `
+    <p>
+        <i>Week 1: Spectral Sequences</i>. The first talk in our reading group! We will define spectral sequences, and use as our motivating example the spectral sequence of a filtered complex. We will then introduce the spectral sequence of a double complex, and produce quick and marvelous proofs of some results in homological algebra, such as the five lemma and the snake lemma.
+    </p>
+    <p>
+        Speaker: <a href="../webpage/index.html">Declan Zammit</a>, Macquarie University
+    </p>
+    <img src="week_1.png" alt="Horizontal filtration on double complex" class = "weekly">
+    <p>
+        The notes and video will be made available after the talk.
+    </p>
+`;
+
 const week2HTML = `
+    <h2>
+    Week 2: Steenrod Squares
+    </h2>
+    <h3>
+    Date and location to be confirmed
+    </h3>
     <p>
     Abstract: We will concretely introduce the Steenrod squares, which are precisely the stable cohomology operations on the modulo 2 cochains of a topological space. The method we use is a combination of the ones used by Mosher-Tangora and Lurie. We then prove some of the basic properties of Steenrod squares, including the Cartan formula and the Adem relations.
     </p>
@@ -24,9 +49,40 @@ const week2HTML = `
     </p>
 `;
 
+const week2HTMLmobile = `
+    <p>
+    <i>Week 2: Steenrod Squares.</i> We will concretely introduce the Steenrod squares, which are precisely the stable cohomology operations on the modulo 2 cochains of a topological space. The method we use is a combination of the ones used by Mosher-Tangora and Lurie. We then prove some of the basic properties of Steenrod squares, including the Cartan formula and the Adem relations.
+    </p>
+    <p>
+    Speaker: <a href="../webpage/index.html">Declan Zammit</a>, Macquarie University
+    </p>
+    <img src="week_2.png" alt="Horizontal filtration on double complex" class = "weekly">
+    <p>
+    The notes and video will be made available after the talk.
+    </p>
+`;
+
 const week3HTML = `
+    <h2>
+    Week 3: Hopf Algebras
+    </h2>
+    <h3>
+    Date and location to be confirmed
+    </h3>
     <p>
     Abstract: To be confirmed.
+    </p>
+    <p>
+    Speaker: To be confirmed.
+    </p>
+    <p>
+    The notes and video will be made available after the talk.
+    </p>
+`;
+
+const week3HTMLmobile = `
+    <p>
+    <i>Week 3: Hopf Algebras.</i> Abstract to be confirmed.
     </p>
     <p>
     Speaker: To be confirmed.
@@ -78,12 +134,24 @@ document.querySelectorAll(".week1").forEach(el => {
   el.innerHTML = week1HTML;
 });
 
+document.querySelectorAll(".week1mobile").forEach(el => {
+  el.innerHTML = week1HTMLmobile;
+});
+
 document.querySelectorAll(".week2").forEach(el => {
   el.innerHTML = week2HTML;
 });
 
+document.querySelectorAll(".week2mobile").forEach(el => {
+  el.innerHTML = week2HTMLmobile;
+});
+
 document.querySelectorAll(".week3").forEach(el => {
   el.innerHTML = week3HTML;
+});
+
+document.querySelectorAll(".week3mobile").forEach(el => {
+  el.innerHTML = week3HTMLmobile;
 });
 
 document.querySelectorAll(".week4").forEach(el => {
