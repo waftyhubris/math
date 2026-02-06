90ca01450f4d39ca9031e4a5d6ea8b4ade744b1c
waftyhubris <declantzammit@gmail.com>
2026-02-07 01:51:30 +1100

    Added ability to select words from footer

---
+++ hieroglyphs/script.js
@@ -0,0 +1,23 @@
+const sentence = document.getElementById("sentence");
+const buttons = document.querySelectorAll("#buttons .word");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const word = button.dataset.word;
+
+    const newButton = document.createElement("button");
+    newButton.className = "word";
+    newButton.textContent = word;
+
+    // remove from sentence AND re-enable source button
+    newButton.addEventListener("click", () => {
+      newButton.remove();
+      button.disabled = false;
+    });
+
+    sentence.appendChild(newButton);
+
+    // disable source button
+    button.disabled = true;
+  });
+});
